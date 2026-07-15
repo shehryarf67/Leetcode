31dbdf23e3fd4e4ea832bb14e186e5b9c5711598
Shehryar Faisal <shehryarf67@gmail.com>
2026-07-15 06:24:23 +0500

    Time: 1373 ms (17.66%) | Memory: 18.1 MB (94.65%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -17,6 +17,10 @@ class Solution(object):
                 sum = nums[i] + nums[j] + nums[k]
                 if sum == 0:
                     result.add((nums[i], nums[j], nums[k]))
+                    while j < k and nums[j] == nums[j + 1]:
+                        j += 1
+                    while j < k and nums[k] == nums[k - 1]:
+                        k -= 1
                     j += 1
                     k -= 1
                 elif sum < 0:
