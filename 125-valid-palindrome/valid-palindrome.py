ed7d554ef7645cc17f6b7b1a457d0eaa2add488c
Shehryar Faisal <shehryarf67@gmail.com>
2026-07-15 05:57:46 +0500

    Time: 7 ms (98.16%) | Memory: 12.7 MB (77.24%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s = s.lower()
+
+        i = 0
+        j = len(s) - 1
+
+        while i < j:
+            while i < j and not s[i].isalnum():
+                i += 1
+            while j > i and not s[j].isalnum():
+                j -= 1
+
+            if s[i] != s[j]:
+                return False
+            
+            i += 1
+            j -= 1
+
+        return True
