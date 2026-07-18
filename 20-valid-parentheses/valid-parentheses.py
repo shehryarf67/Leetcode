d478b9f7fce8cb31f91246e53351ca2d4f688c6f
Shehryar Faisal <shehryarf67@gmail.com>
2026-07-18 12:08:32 +0500

    Time: 3 ms (74.13%) | Memory: 12.4 MB (95.87%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+
+        for i in s:
+            if i == '(' or i == '{' or i == '[':
+                stack.append(i)
+            else:
+                if not stack:
+                    return False
+                top = stack.pop()
+                if i == ')' and top != '(' or i == '}' and top != '{' or i == ']' and top != '[':
+                    return False
+
+        return not stack
