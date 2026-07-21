03efccf31d6287764bc01b04acc07be59232952c
Shehryar Faisal <shehryarf67@gmail.com>
2026-07-21 06:47:37 +0500

    Time: 0 ms (100.00%) | Memory: 13.2 MB (70.41%) - LeetSync

---
+++ 792-binary-search/binary-search.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        i = 0
+        j = len(nums) - 1
+
+        while i <= j:
+            mid = (i + j) // 2
+
+            if target > nums[mid]:
+                i = mid + 1
+            elif target < nums[mid]:
+                j = mid - 1
+            else:
+                return mid
+            
+        return -1
