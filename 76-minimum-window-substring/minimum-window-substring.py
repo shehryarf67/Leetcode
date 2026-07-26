1ae05d82020449b705a8c0c76bb1b60b4719741a
Shehryar Faisal <shehryarf67@gmail.com>
2026-07-26 07:19:58 +0500

    Time: 170 ms (26.27%) | Memory: 15.8 MB (22.56%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -0,0 +1,43 @@
+class Solution(object):
+    def minWindow(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        if len(t) > len(s):
+            return ""
+
+        count = defaultdict(int)
+
+        for c in t:
+            count[c] += 1
+
+        left = 0
+        needed = len(t)
+        minLength = float('inf')
+        result = ""
+
+        for right in range(len(s)):
+            if count[s[right]] > 0:
+                needed -= 1
+
+            count[s[right]] -= 1
+
+            while needed == 0:
+                # Add the string to result only if the length is lesser than the current
+                if right - left + 1 < minLength:
+                    minLength = right - left + 1
+                    result = s[left:right + 1]
+
+                count[s[left]] += 1
+                # Increment the occurrence, if it's non-t, it will always be <= 0
+                # So only for t letters it can become > 0
+                if count[s[left]] > 0:
+                    needed += 1
+
+                left += 1
+
+        return result
+
+            
