3f26422d80e1f8b00427e29dc75d5d63cfb1effd
Shehryar Faisal <shehryarf67@gmail.com>
2026-07-26 06:42:46 +0500

    Time: 23 ms (72.57%) | Memory: 12.9 MB (16.28%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def checkInclusion(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        if len(s1) > len(s2):
+            return False
+
+        count = defaultdict(int)
+
+        for c in s1:
+            count[c] += 1
+
+        left = 0
+        needed = len(s1)
+
+        for right in range(len(s2)):
+            if count[s2[right]] > 0:
+                needed -= 1
+
+            count[s2[right]] -= 1
+
+            if needed == 0:
+                return True
+
+            if right - left + 1 == len(s1):
+                if count[s2[left]] >= 0:
+                    needed += 1
+
+                count[s2[left]] += 1
+                left += 1
+
+        return False
