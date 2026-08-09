73fead5e120b7ad212573ce28ac40d8fa002659b
Shehryar Faisal <shehryarf67@gmail.com>
2026-08-09 06:37:52 +0500

    Time: 207 ms (80.33%) | Memory: 48.3 MB (54.07%) - LeetSync

---
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def goodNodes(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        numNodes = [0]
+
+        def dfs(node, maxSoFar):
+            if not node:
+                return
+
+            if node.val >= maxSoFar:
+                numNodes[0] += 1
+
+            maxSoFar = max(maxSoFar, node.val)
+
+            dfs(node.left, maxSoFar)
+            dfs(node.right, maxSoFar)
+
+        dfs(root, root.val)
+
+        return numNodes[0]
