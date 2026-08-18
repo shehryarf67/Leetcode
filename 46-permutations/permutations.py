bdd43e7c8f4004e39658a46faa661d26a89512fb
Shehryar Faisal <shehryarf67@gmail.com>
2026-08-18 13:21:55 +0500

    Time: 0 ms (100.00%) | Memory: 12.3 MB (99.09%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        combinations = []
+        path = []
+
+        def backtrack(start, path, length):
+            if length == len(nums):
+                combinations.append(path[:])
+                return 
+
+            for i in range(len(nums)):
+                if nums[i] not in path:
+                    path.append(nums[i])
+                    backtrack(i + 1, path, len(path))
+                    path.pop()
+
+        backtrack(0, [], 0)
+        return combinations
