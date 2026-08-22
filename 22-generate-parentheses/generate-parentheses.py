681d503d98f6d4a2f8b3bfe98077b935df3b449c
Shehryar Faisal <shehryarf67@gmail.com>
2026-08-22 14:58:39 +0500

    Time: 4 ms (20.45%) | Memory: 12.7 MB (19.14%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        combinations = []
+        string = ""
+
+        def backtrack(string, forwardBrack, backwardBrack):
+            if len(string) == n * 2:
+                combinations.append(string)
+                return
+
+            if forwardBrack < n:
+                backtrack(string + "(", forwardBrack + 1, backwardBrack)
+
+            if backwardBrack < forwardBrack:
+                backtrack(string + ")", forwardBrack, backwardBrack + 1)
+
+        backtrack(string, 0, 0)
+        return combinations
