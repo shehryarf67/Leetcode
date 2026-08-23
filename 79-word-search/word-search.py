9eb9080f2e03bab5ff59d1bae896cf6cff5e36b3
Shehryar Faisal <shehryarf67@gmail.com>
2026-08-23 10:50:03 +0500

    Time: 6785 ms (21.37%) | Memory: 12.3 MB (57.95%) - LeetSync

---
+++ 79-word-search/word-search.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    result = False
+    def exist(self, board, word):
+        """
+        :type board: List[List[str]]
+        :type word: str
+        :rtype: bool
+        """
+        path = ""
+        def backtrack(path, i, j):
+            if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]):
+                return
+
+            if board[i][j] != word[len(path)]:
+                return
+
+            path += board[i][j]
+
+            if path == word:
+                self.result = True
+                return
+
+            temp = board[i][j]
+            board[i][j] = "#"
+
+            backtrack(path, i, j + 1)  # right
+            backtrack(path, i, j - 1)  # left
+            backtrack(path, i + 1, j)  # down
+            backtrack(path, i - 1, j)  # up
+
+            board[i][j] = temp
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                backtrack(path, i, j)
+
+                if self.result:
+                    return True
+
+        return False
